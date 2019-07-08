68a7ed58b6b31fffbed76cc69e1a47ae98fbaf9e
rossgales <ross.gales@gmail.com>
2019-07-08 21:43:46 +0100

    notebook settings

--- homesite/settings.py
+++ homesite/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+NOTEBOOK_ARGUMENTS = [
+    '--ip=127.0.0.1',
+    '--port=8888',
+    '--no-browser',
+]
