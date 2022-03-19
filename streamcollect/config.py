6bc735fa5c3b73510de468b37f01594222aa946c
Ross <17883828+rosscg@users.noreply.github.com>
2022-03-19 19:04:15 +0000

    formatting

--- streamcollect/config.py
+++ streamcollect/config.py
@@ -1,40 +1,42 @@
 ### Data Collection ###
 
-# Choose whether to download media from Tweets to hard drive. Saves photos, videos and gifs from Twitter and Instagram posts to protect against deletion by the author.
+# Choose whether to download media from Tweets to hard drive. Saves photos,
+# videos and gifs from Twitter and Instagram posts to protect against deletion
+# by the author or Twitter, or profiles made private.
 DOWNLOAD_MEDIA = True
 
-# Process a proportion of Tweets in the low-priority keyword stream. This must be manually adjusted to match the systems limits.
-#STREAM_PROPORTION = 0.50
-
-# Thresholds for determining spam/celebrity/news accounts (to reject)
+# Thresholds for determining spam/celebrity/news accounts
+# These may be used to auto-reject to reduce load by adjusting code.
+# Currently used for post-collection classification.
 FOLLOWERS_THRESHOLD = 5000
 FRIENDS_THRESHOLD = 5000
 STATUSES_THRESHOLD = 10000
 
 # Ignore Tweets by keyword:
-IGNORED_KWS = ['pray', '🙏']
+IGNORED_KWS = ['pray', '🙏'] # Chosen based on hurricane study
 # Ignore from following sources:
 IGNORED_SOURCES = ['Paper.li', 'TweetMyJOBS', 'CareerArc'] # These sources have been identified as automated accounts which do not contribute useful information, so were ignored in the original application of this tool. Adjust as necessary.
 
-# Ignore Re-Tweets. Note that keywords will match phrases in the ancestor Tweet, but geo-stream
-# will not detect coordinates in ancestor (nor are RTs ever assigned coordinates). Enabling RTs
-# requires more efficient handling to be implemented.
+# Ignore Re-Tweets. Note that keywords will match phrases in the ancestor Tweet,
+# but geo-stream will not detect coordinates in ancestor (nor are RTs ever
+# assigned coordinates). Enabling RTs increases load and reduces original content.
 IGNORE_RTS = True
 
-# When a streamed Tweet is a reply, this sets how far 'up the chain' to record. A high value may create too much load.
+# When a streamed Tweet is a reply, this sets how far 'up the chain' to record.
+# A high value may create too much load.
 MAX_REPLY_DEPTH = 1
 
 # Seconds between each keyword stream reset to get fresh keyword list.
 REFRESH_STREAM = False #TODO: No longer needed, remove in twdata
 STREAM_REFRESH_RATE = None # in seconds
 
-# Parameters of bounding box for GPS stream if single point coordinates are provided.
+# Parameters of bounding box for GPS stream if single point coords are provided.
 BOUNDING_BOX_WIDTH = 1.2
 BOUNDING_BOX_HEIGHT = 0.6
 
 # Threshold proportion for a hashtag to appear before it is added to tracked tags.
 TAG_OCCURENCE_THRESHOLD = 0.02
-# Threshold proportion for a mentioned user to appear before it is added as user_class=2
+# Threshold proportion for a mentioned user to appear before added as user_class=2
 MENTION_OCCURENCE_THRESHOLD = 0.01
 
 
